e4cf64c16587598f4897cc65ce5360b4d53a69e5
notaarryan <aryanparmar2503@gmail.com>
2024-08-20 20:40:31 +0530

    Billing.java completed

--- src/Billing.java
+++ src/Billing.java
@@ -2,16 +2,25 @@ public class Billing {
 
     public static double[] computePaymentAmount(Patient patient, double amount) {
         double[] payments = new double[2];
+        double paymentByInsurance = 0.0;
 
         HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
 
-        if (patientInsurancePlan == null) {
-            payments[0] = 0.0;
-            payments[1] = amount;
+        if(patientInsurancePlan != null) {
+            paymentByInsurance = patientInsurancePlan.getCoverage()*amount;
+            payments[0] = paymentByInsurance;
+        }
+
+        if (patientInsurancePlan instanceof PlatinumPlan) {
+            payments[1] = amount - paymentByInsurance - 50;
+        } else if (patientInsurancePlan instanceof GoldPlan) {
+            payments[1] = amount - paymentByInsurance - 40;
+        } else if (patientInsurancePlan instanceof SilverPlan) {
+            payments[1] = amount - paymentByInsurance - 30;
+        } else if (patientInsurancePlan instanceof BronzePlan) {
+            payments[1] = amount - paymentByInsurance - 25;
         } else {
-            double coverage = patientInsurancePlan.getCoverage();
-            payments[0] = coverage;
-            payments[1] = amount-coverage;
+            payments[1] = amount - 20;
         }
 
         return payments;
