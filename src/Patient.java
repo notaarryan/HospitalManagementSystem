b99349f55091eeaf1240bfab530aa1b616036bb4
notaarryan <aryanparmar2503@gmail.com>
2024-08-20 17:06:37 +0530

    variables added to Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,20 @@
-public class Patient {
+public class Patient extends User{
+    private long patientId;
+    private boolean insured;
+
+    public long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(long patientId) {
+        this.patientId = patientId;
+    }
+
+    public boolean isInsured() {
+        return insured;
+    }
+
+    public void setInsured(boolean insured) {
+        this.insured = insured;
+    }
 }
